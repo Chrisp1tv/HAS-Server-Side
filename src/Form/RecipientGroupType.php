d2b87e2c0490e66cb9ddd29de5d2d7b17c6d3867
Christopher Anciaux <christopher.anciaux@gmail.com>
2018-01-17 09:33:20 +0100

    Wrote some views, and started to write styles

--- src/Form/RecipientGroupType.php
+++ src/Form/RecipientGroupType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -26,6 +27,9 @@ class RecipientGroupType extends AbstractType
                 'label'    => 'form.label.recipients',
                 'multiple' => true,
                 'required' => true,
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'form.label.save',
             ));
     }
 
--- src/Form/RecipientType.php
+++ src/Form/RecipientType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -24,6 +25,9 @@ class RecipientType extends AbstractType
             ->add('linkingIdentifier', TextType::class, array(
                 'label'    => 'form.label.linkingIdentifier',
                 'disabled' => true,
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'form.label.save',
             ));
     }
 
