c1310cd36ccfe14a922f3be1ef997d1f5fb5342a
Christopher Anciaux <christopher.anciaux@gmail.com>
2018-02-03 21:45:06 +0100

    Started design Added logout config, corrected typo in Administrator routes file

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
     .addStyleEntry('css/app', './assets/css/styles.scss')
+    .addEntry('images/favicon', './assets/images/favicon.ico')
+    .addEntry('images/main-logo', './assets/images/main-logo.png')
     .enableSassLoader()
 ;
 
